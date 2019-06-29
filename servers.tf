6b881affda4bf7e7a8b2dc132694b56ed2cd5fab
=Amirali <amirali7089@gmail.com>
2019-06-29 18:54:11 +0000

    Add roles to servers file
    Signed-off-by: =Amirali <amirali7089@gmail.com>

--- servers.tf
+++ servers.tf
@@ -21,10 +21,35 @@ resource "aws_instance" "instance-01" {
     encrypted = true
   }
 
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.name}"
   
  
   tags = {
     Name = "terraform-instance"
   }
  
+}
+
+
+
+resource "aws_iam_role" "ec2_firehose_access_role" {
+  name               = "firehose-role-tf"
+  assume_role_policy = "${file("role-policy.json")}"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "firehose-access-policy"
+  description = "A policy to allow full access to firehose"
+  policy      = "${file("policy-firehose.json")}"
+}
+
+resource "aws_iam_policy_attachment" "policy-attach" {
+  name       = "policy-attachment"
+  roles      = ["${aws_iam_role.ec2_firehose_access_role.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = "${aws_iam_role.ec2_firehose_access_role.name}"
 }
